00d4b67728593b5aac879472fe0d444663918681
Prudvi_Nelaturu <124529501+Prudvi970@users.noreply.github.com>
2026-07-17 22:27:42 +0530

    Add files via upload

---
+++ Java_Topics_Wise_Programs/Arrays/FindElements.java
@@ -0,0 +1,11 @@
+class FindElements{
+    public static void main(String[] args){
+
+        int[] arr = {10,20,30,40,50};
+         System.out.println("This is the printing all elements in array");
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+       
+    }
+} 
---
+++ Java_Topics_Wise_Programs/Arrays/SumOfElements.java
@@ -0,0 +1,13 @@
+class SumOfElements{
+    public static void main(String[] args){
+        int sum = 0;
+        int[] arr = {10,20,30,40,50};
+        
+        for(int i = 0; i < arr.length; i++){
+          
+           sum += arr[i];
+          
+        }
+        System.out.println(sum);
+    }
+}
